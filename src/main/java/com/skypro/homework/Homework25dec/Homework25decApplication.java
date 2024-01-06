21a802faf5841332b75e638df50952c5c74e2e93
miksor2023 <miksor@yandex.ru>
2024-01-06 17:45:58 +0300

    Initial commit

---
+++ src/main/java/com/skypro/homework/Homework25dec/Homework25decApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.homework.Homework25dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework25decApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework25decApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/homework/Homework25dec/Homework25decApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.homework.Homework25dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework25decApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
